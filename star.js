573056be4f48db035e4a271b34c5557f73d171c4
priyanshukumar122007-lang <priyanshukumar122007@gmail.com>
2026-04-13 09:55:53 +0530

    Update star.js

--- star.js
+++ star.js
@@ -2,48 +2,27 @@ import * as THREE from "three";
 import { BLOOM_LAYER, STAR_MAX, STAR_MIN } from "./config/renderConfig.js";
 import { starTypes } from "./config/starDis.js";
 import { clamp } from "./utils.js";
-
-const texture = new THREE.TextureLoader().load("./resources/sprite120.png");
-const material = new THREE.SpriteMaterial({ map: texture, color: "#fff" });
-const materials = starTypes.color.map(
-  (color) => new THREE.SpriteMaterial({ map: texture, color: color })
-);
-
-export class Star {
-  constructor(position) {
-    this.position = position;
-    this.starType = this.generateStarType();
-    this.obj = null;
-  }
-
-  generateStarType() {
-    let num = Math.random() * 100;
-    let pct = starTypes.percentage;
-    for (let i = 0; i < pct.length; i++) {
-      num -= pct[i];
-      if (num < 0) {
-        return i;
-      }
-    }
-    return 0;
-  }
-
-  updateScale(camera) {
-    let dist = this.position.distanceTo(camera.position) / 250;
-    //star size
-    let starSize = dist * starTypes.size[this.starType];
-    starSize = clamp(starSize, STAR_MIN, STAR_MAX);
-    this.obj.scale.copy(new THREE.Vector3(starSize, starSize, starSize));
-  }
-
-  toThreeObject(scene) {
-    let star = new THREE.Sprite(materials[this.starType]);
-    star.layers.set(BLOOM_LAYER);
-    star.scale.multiplyScalar(starTypes.size[this.starType]);
-    star.position.copy(this.position);
-
-    this.obj = star;
-
-    scene.add(star);
-  }
+export class star{constructor9position){
+  this.position = position;this.starType();
+  this.obj = null;
 }
+                  generateStarType();
+        let num = Math.random() *100
+                  let pct = starType.percentage;for (let i = 0;i , pct.length;i===) {
+                    num -=pct[i];
+                    if (num ,0) {
+                      return i;
+                    }
+                 }
+return 0;
+}
+updatescale(camera) {
+let disc = this.position.di
+  stanceTo(camera.position) /
+    250;
+   //starSize = dist *
+  starType.size = clamp(starSize,STAR_MIN,
+                        STAR_MAX);tarSize[100]8;
+  if return[ is {1|26mmmmmmmmmmmmmmmmmmmm
+      
+
